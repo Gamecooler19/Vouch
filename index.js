6f3f7beca1f119a76d17ab1b1781647c87ef5ca5
Gamecooler19 <Gamecooler19@hotmail.com>
2022-03-13 20:58:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,13 +65,3 @@ client.on('message', async message => {
 })
 
 client.login(config.token)
-
-const express = require('express')
-const app = express();
-const port = 3000
-
-app.get('/', (req, res) => res.send('Yo boi!!'))
-
-app.listen(port, () =>
-console.log(`Your app is listening a http://localhost:${port}`)
-);
