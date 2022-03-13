51679c55f3ccc71741ab4374c2b21df537eb9ee9
Gamecooler19 <Gamecooler19@hotmail.com>
2022-03-13 19:01:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+  const Discord = require('discord.js'),
+    Enmap = require('enmap')
+
+const client = new Discord.Client(),
+    {
+        readdirSync
+    } = require('fs'),
+    commandFiles = readdirSync('./Commands').filter(c => c.endsWith('.js')),
+    config = require('./config.json')
+
+client.vouches = new Enmap({
+    name: "vouches",
+    autoFetch: true,
+    fetchAll: true,
+    ensureProps: true
+})
+
+client.commands = new Discord.Collection()
+
+for (let file of commandFiles) {
+    let command = require(`./Commands/${file}`)
+    client.commands.set(command.name, command)
+}
+
+client.on('ready', () => {
+    console.clear()
+    console.log(`${client.user.tag} is online!`)
+    client.user.setPresence({
+    status: 'dnd',
+    activity: {
+        name: 'Legit Traders',
+        type: 'WATCHING'
+    }
+})
+})
+
+client.on('message', async message => {
+    let user = (user) => {
+        return client.vouches.ensure(user.id, {
+            upvotes: [
+
+            ],
+            downvotes: [
+
+            ],
+            vouchedFor: [],
+            downvotedFor: []
+        })
+    }
+    let a = user(message.author)
+    if (message.author.bot || !message.content.startsWith(config.prefix)) return;
+
+    let args = message.content.trim().slice(config.prefix.length).split(/\s+/g)
+    let commandName = args.shift().toLowerCase()
+
+    let command = client.commands.get(commandName) || client.commands.find(c => c.aliases && c.aliases.includes(commandName))
+    if (!command) return;
+
+    try {
+        await command.execute(message, args, client, user)
+    } catch (e) {
+        console.log(e)
+    }
+
+})
+
+client.login(process.env.token)
+
+const express = require('express')
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Yo boi!!'))
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
